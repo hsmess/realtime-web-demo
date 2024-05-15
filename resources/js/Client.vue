f2c6ea2c96056ca50528ae13617f406661c59db7
Harry Messenger <harrymessenger@Harrys-MacBook-Air.local>
2024-05-15 15:02:24 +0100

    mancweb->leeds

--- resources/js/Client.vue
+++ resources/js/Client.vue
@@ -59,7 +59,7 @@ import 'vue3-toastify/dist/index.css';
                                             <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email</label>
                                             <div class="mt-2">
                                                 <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                                                    <input  v-model="email" type="email" name="email" id="email" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="mancweb@hsmess.dev" />
+                                                    <input  v-model="email" type="email" name="email" id="email" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="phpleeds@hsmess.dev" />
                                                 </div>
                                             </div>
                                         </div>
